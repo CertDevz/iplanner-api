3d6ba6d2f216160f868978d9400a943c102a9e3e
Rafael Torreson <rafael.torreson@hotmail.com>
2024-06-28 14:45:40 -0300

    feat: api to list data

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+import routes from './routes.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+
+app.use('/', routes);
+
+app.listen(3333, () => {
+  console.log('Servidor rodando na porta 3333');
+});
---
+++ routes.js
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient();
+const router = Router();
+
+function FormatDate(date) {
+  const day = new Date(date).getDate();
+  const month = new Date(date).getMonth();
+  const year = new Date(date).getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
+
+router.get('/cursos', async (request, response) => {
+  const query = await db.events.findMany();
+  let data = [];
+
+  query.forEach((dataDb) => {
+    const format = {
+      id: dataDb.id,
+      title: dataDb.title,
+      image: dataDb.backgroundImage,
+      description: FormatDate(dataDb.date),
+    };
+
+    data.push(format);
+  });
+
+  return response.status(200).json(data);
+});
+
+router.get('/curso/:id', async (request, response) => {
+  const query = await db.events.findUnique({
+    where: {
+      id: request.params.id,
+    },
+  });
+
+  if (!query) {
+    response.status(404).json('Evento não encontrado');
+  }
+
+  return response.status(200).json(query);
+});
+
+export default router;
